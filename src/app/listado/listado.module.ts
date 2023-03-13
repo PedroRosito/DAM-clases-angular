3ed9808f80906d480bcf38dc2271f7c9338be95b
pedro <rositopedro@gmail.com>
2023-03-13 08:47:19 -0300

    Se importan pipe y directiva custom

--- src/app/listado/listado.module.ts
+++ src/app/listado/listado.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListadoComponent } from './listado.component';
 import { FormsModule } from '@angular/forms';
+import { ElevarPipe } from '../pipes/elevar.pipe';
+import { ColorearDirective } from '../directives/colorear.directive';
 
 
 
 @NgModule({
-  declarations: [ListadoComponent],
+  declarations: [
+    ListadoComponent,
+    ElevarPipe,
+    ColorearDirective,
+  ],
   imports: [
     CommonModule,
     FormsModule
