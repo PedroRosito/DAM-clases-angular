ad81973cebbd4925ca063f38a5148f4a88a57690
pedro <rositopedro@gmail.com>
2023-03-26 17:07:20 -0300

    Se añaden archivos de estilos, vista y test del componente dispositivo

---
+++ src/app/dispositivo/dispositivo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DispositivoComponent } from './dispositivo.component';
+
+describe('DispositivoComponent', () => {
+  let component: DispositivoComponent;
+  let fixture: ComponentFixture<DispositivoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DispositivoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DispositivoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
